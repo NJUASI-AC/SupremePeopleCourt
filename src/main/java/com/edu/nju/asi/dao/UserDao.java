5a9fc7015216a5b9536cc27ac6a256eca4ce9c9f
Harvey Gong <335931662@qq.com>
2017-07-17 12:57:26 +0800

    Delete: 测试所用用户类

--- src/main/java/com/edu/nju/asi/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.edu.nju.asi.dao;
-
-import com.edu.nju.asi.model.User;
-
-/**
- * Created by Administrator on 2017/7/16 0016.
- */
-public interface UserDao extends MongoBaseDao<User> {
-}
