e7a2ed3bc284c61b26e52bd0aafcbe58c55caaf0
ByronDong <2104869365@qq.com>
2017-07-21 10:20:10 +0800

    修改DataTest

--- src/test/java/com/edu/nju/asi/dao/daoImpl/DataManagerDaoImplTest.java
+++ src/test/java/com/edu/nju/asi/dao/daoImpl/DataManagerDaoImplTest.java
@@ -6,12 +6,16 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Byron Dong on 2017/7/19.
  */
 public class DataManagerDaoImplTest {
+
 
     private DataManagerDao dataManagerDao;
 
@@ -26,8 +30,12 @@ public class DataManagerDaoImplTest {
         dataManagerDao.getRecommendCase("9018");
     }
 
+    @Ignore
     @Test
     public void getCase() throws Exception {
+        List<String> caseIDs = new ArrayList<>();
+        caseIDs.add("(2001)南民初字第5905号民事判决书（一审民事案件用）");
+        dataManagerDao.getCase(caseIDs);
     }
 
 }
