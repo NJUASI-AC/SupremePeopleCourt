5cc504c442f34b1a2b17ff4f041481afe35469c7
ByronDong <2104869365@qq.com>
2017-07-25 13:39:25 +0800

    优化数据查询速度

--- src/main/java/com/edu/nju/asi/dao/daoImpl/CaseDaoImpl.java
+++ src/main/java/com/edu/nju/asi/dao/daoImpl/CaseDaoImpl.java
@@ -3,8 +3,11 @@ package com.edu.nju.asi.dao.daoImpl;
 import com.edu.nju.asi.InfoCarrier.RecommendCase;
 import com.edu.nju.asi.dao.CaseDao;
 import com.edu.nju.asi.model.Case;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
@@ -72,10 +75,13 @@ public class CaseDaoImpl implements CaseDao {
      */
     @Override
     public List<RecommendCase> findAll(String actionCode) {
-        Query query = new Query(Criteria.where("proceedings.mainActionCause.actionCode").is(actionCode));
+        DBObject dbObject = new BasicDBObject();
+        dbObject.put("proceedings.mainActionCause.actionCode",actionCode);
+        BasicDBObject basicDBObject = this.createRecommendObject();
+        Query query = new BasicQuery(dbObject,basicDBObject);
         List<Case> caseList = mongoTemplate.find(query, Case.class, collectionName);
         List<RecommendCase> recommendCases = new ArrayList<>();
-        for(Case case_need:caseList){
+        for (Case case_need : caseList) {
             recommendCases.add(new RecommendCase(case_need));
         }
         return recommendCases;
@@ -89,8 +95,8 @@ public class CaseDaoImpl implements CaseDao {
      */
     @Override
     public List<RecommendCase> getRecommendCase(List<String> actionCodes) {
-        List<RecommendCase> recommendCases =  new ArrayList<>();
-        for(String actionCode: actionCodes){
+        List<RecommendCase> recommendCases = new ArrayList<>();
+        for (String actionCode : actionCodes) {
             recommendCases.addAll(this.findAll(actionCode));
         }
         return recommendCases;
@@ -104,14 +110,33 @@ public class CaseDaoImpl implements CaseDao {
      */
     @Override
     public List<Case> getCase(List<String> caseIDS) {
-       List<Case> caseList = new ArrayList<>();
-       for(String caseID: caseIDS){
-           Query query = new Query(Criteria.where("caseID").is(caseID));
-           Case case_need = mongoTemplate.findOne(query, Case.class, collectionName);
-           if(case_need !=null){
-               caseList.add(case_need);
-           }
-       }
-       return caseList;
+        List<Case> caseList = new ArrayList<>();
+        for (String caseID : caseIDS) {
+            Query query = new Query(Criteria.where("caseID").is(caseID));
+            Case case_need = mongoTemplate.findOne(query, Case.class, collectionName);
+            if (case_need != null) {
+                caseList.add(case_need);
+            }
+        }
+        return caseList;
+    }
+
+    /**
+     * 获取推荐案例最小集
+     *
+     * @return BasicDBObject
+     */
+    private BasicDBObject createRecommendObject(){
+        BasicDBObject basicDBObject = new BasicDBObject();
+        basicDBObject.put("caseID",true);
+        basicDBObject.put("header.caseNum",true);
+        basicDBObject.put("header.handlingCourt",true);
+        basicDBObject.put("header.nameOfDocument",true);
+        basicDBObject.put("proceedings.records",true);
+        basicDBObject.put("proceedings.mainActionCause",true);
+        basicDBObject.put("proceedings.extraActionCause",true);
+        basicDBObject.put("caseBasic.paragraphThisTrial",true);
+        basicDBObject.put("refereeAnalysisProcess",true);
+        return basicDBObject;
     }
 }
--- src/test/java/com/edu/nju/asi/dao/daoImpl/CaseDaoImplTest.java
+++ src/test/java/com/edu/nju/asi/dao/daoImpl/CaseDaoImplTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -27,19 +28,28 @@ public class CaseDaoImplTest {
     @Ignore
     @Test
     public void find() throws Exception {
+        long start = System.currentTimeMillis();
         Case case_need = caseDao.find("C__Users_Administrator_Desktop_刑事二审案件_刑事二审案件_100000");
+        System.out.println(System.currentTimeMillis()-start);
         System.out.println("ok");
     }
 
     @Ignore
     @Test
     public void findAll() throws Exception {
+        long start = System.currentTimeMillis();
         List<RecommendCase> recommendCases =  caseDao.findAll("200");
+        System.out.println(System.currentTimeMillis()-start);
         System.out.println(recommendCases.size());
     }
 
     @Test
     public void getRecommendCase() throws Exception {
+        List<String> codes = new ArrayList<>();
+        codes.add("200");
+        long start = System.currentTimeMillis();
+        caseDao.getRecommendCase(codes);
+        System.out.println(System.currentTimeMillis()-start);
     }
 
     @Test
